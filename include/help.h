5f5018b214163abe4f5a53b0523f122381e2b1dd
Rusell Crandall <russ@monolith>
1998-12-22 15:24:57 +0000

    changed online_help_wrapper from static to extern.

--- include/help.h
+++ include/help.h
@@ -10,3 +10,4 @@ extern void help_topics( void );
 extern void literature_menu( void );
 extern int help_commands( void );
 extern void online_help(const char);
+extern void online_help_wrapper(const unsigned int, const long, const char *);
