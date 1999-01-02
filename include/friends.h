0a10386c2f853194ec3eabbf0ca48a5493aa62a3
Rusell Crandall <russ@monolith>
1999-01-02 21:54:12 +0000

    header for new cache stuff.

--- include/friends.h
+++ include/friends.h
@@ -5,8 +5,9 @@ extern void menu_friend( int param );
 extern int is_my_friend( const char *name );
 extern int is_my_enemy( const char *name );
 extern void friends_online( void );
-extern void update_friends_cache(void);
+/*extern void update_friends_cache(void);
 extern char * cached_x_to_name(const int);
 extern int cached_name_to_x(const char *);
 extern int is_cached_friend(const char *);
+*/
 /* eof */
