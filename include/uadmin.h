b9721919692fa23b41a4c2fc379f74371b8878e5
Rusell Crandall <russ@monolith>
1999-01-04 22:31:05 +0000

    wrote namechenge function

--- include/uadmin.h
+++ include/uadmin.h
@@ -6,4 +6,4 @@ extern void DELETE_DELMARKED_USERS( void );
 extern void update_all_users_read_from_userdir( void );
 extern void deluserfromw (void);      
 extern int log_sysop_action ( const char *event,...);
-
+extern void namechange(void);
