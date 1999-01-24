24c1b29c1ad8e9e99a9725a7e7571147bb6213c2
Michel Oosterhof <michel@oosterhof.net>
1999-01-24 22:08:43 +0000

    ql_sql works

--- misc/ql_sql.c
+++ misc/ql_sql.c
@@ -11,6 +11,8 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <mysql.h>
+
 #include "monolith.h"
 
 int main(void);
@@ -20,24 +22,40 @@ main()
 {
 
     int fd1, i, j, h;
+    size_t ret;
     room_t old;
     user_t u;
-    fd1 = open( BBSDIR "save/fastquadrant", O_RDONLY);
+    MYSQL_RES *res;
+    fd1 = open(BBSDIR "save/fastquadrant", O_RDONLY);
 
-
+    if (fd1 == -1) {
+	fprintf(stderr, "can't open fastquadrant");
+	exit(1);
+    }
     mono_sql_connect();
 
-    for (i = 0; i < 150; i++) {
+    printf( "got here\n" );fflush(stdout);
 
-	read(fd1, &old, sizeof(room_t));
-        for( j = 0; j < 5; j++ ) {
-            if(check_user(old.qls[i])) {
-                u = readuser(old.qls[j]);
-                h = u.usernum;
-                mono_sql_query("INSERT INTO userforum VALUES(%d,%d,'y','normal','0')", i, h);
-            }
-        }
-       
+    for (i = 0; i < MAXQUADS ; i++) {
+        printf( "reading quad %d...", i );fflush(stdout);
+
+	ret = read(fd1, &old, sizeof(room_t));
+        if ( ret != sizeof( room_t )  ) {
+              fprintf( stderr, "can't read from fastquadrant" );
+            exit (1 );
+       }
+        printf( "read.." );fflush(stdout);
+	for (j = 0; j < 5; j++) {
+	    if (old.qls[j] && strlen(old.qls[j]))
+		if (check_user(old.qls[j])) {
+                    printf( "ql %d exists: ", j ); fflush(stdout);
+		    if (mono_sql_u_name2id(old.qls[j], &h) != -1)
+   		        printf( "id %u, ",  h );
+			mono_sql_query( &res, "UPDATE userforum SET host='y' WHERE user_id=%u AND forum_id=%u", h, i);
+		}
+	}
+        printf( "done\n" );fflush(stdout);
+
     }
     mono_sql_detach();
     close(fd1);
