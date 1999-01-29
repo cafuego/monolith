02157f2e4ff4fee763c7deddab94c493d4e47c56
Peter Lieverdink <me@cafuego.net>
1999-01-29 23:58:48 +0000

    Renamed functions, added get_unread()...

--- lib/sql_message.c
+++ lib/sql_message.c
@@ -1,39 +1,304 @@
 /* $Id$ */
-/* operatoins we can do on users */
+/* message subsystem */
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <mysql.h>
+#include <time.h>
 
 #include "monolith.h"
+#include "routines.h"
 #include "monosql.h"
 #include "sql_utils.h"
 
 #define M_TABLE "message"
 
+static int _mono_sql_mes_add_num_to_list(mlist_t entry, mlist_t ** list);
+
 int
-mono_sql_add_message()
+mono_sql_mes_get_unread(unsigned int forum, unsigned int lastseen)
 {
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+    int ret = 0;
+
+    ret = mono_sql_query(&res, "SELECT COUNT(*) FROM " M_TABLE " WHERE message_id>%d AND forum_id=%d", lastseen, forum);
+
+    if (ret == -1) {
+        (void) mysql_free_result(res);
+        return -1;
+    }
+
+    row = mysql_fetch_row(res);
+    ret = atoi(row[0]);
+
+    (void) myqsl_free_result(res);
+
+    return ret;
+}
+
+int
+mono_sql_mes_add(message_t *message, unsigned int forum)
+{
+    MYSQL_RES *res;
+    int ret = 0;
+    char *alias = NULL, *subject = NULL, *content = NULL, date[20];
+  
+    if( (message->num = mono_sql_f_get_new_message_id(forum)) == 0)
+        return -1;
+
+    (void) escape_string(message->alias, &alias);
+    (void) escape_string(message->subject, &subject);
+
+    /*
+     * We add the date here, so it really is the date the message was
+     * added to the BBS. TIMESTAMP in the table would be better though.
+     */
+    message->date = time(0);
+    (void) time_to_datetime(message->date, &date);
+
+    ret = mono_sql_query(&res, "INSERT INTO " M_TABLE " (message_id,topic_id,forum_id,author,alias,subject,date,type,priv,deleted) VALUES (%u,%u,%u,%u,'%s','%s','%s',%d,%d,'n')",
+        message->num, message->topic, message->forum, message->author,
+        alias, subject, date, message->type, message->priv );
+
+    (void) mysql_free_result(res);
+    xfree(alias);
+    xfree(subject);
+
+    /*
+     * Add the content now, so we have a bigger content
+     * length due to the fixed size of the query.
+     */
+    (void) escape_string(message->content, &content);
+    ret = mono_sql_query(&res, "UPDATE " M_TABLE " SET content='%s' WHERE message_id=%d AND forum_id=%d", content, num, forum );
+    xfree(content);
+
+    return ret;
+
+}
+
+int
+mono_sql_mes_mark_deleted(unsigned int id, unsigned int forum)
+{
+    MYSQL_RES *res;
+    int ret = 0;
+
+    ret = mono_sql_query(&res, "UPDATE " M_TABLE " SET deleted='y' WHERE message_id=%d AND forum_id=%d", id, forum );
+
+    (void) mysql_free_result(res);
+
+    return ret;
+}
+    
+
+int
+mono_sql_mes_remove(unsigned int id, unsigned int forum)
+{
+    MYSQL_RES *res;
+    int ret = 0;
+
+    ret = mono_sql_query(&res, "DELETE FROM " M_TABLE " WHERE message_id=%d AND forum_id=%d", id, forum);
+    (void) mysql_free_result(res);
+
+    return ret;
+}
+
+int
+mono_sql_mes_retrieve(unsigned int id, unsigned int forum, message_t *data)
+{
+
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+    message_t message;
+    int ret = 0;
+
+    ret = mono_sql_query(&res, "SELECT * FROM " M_TABLE " WHERE message_id=%d AND forum_id=%d", id, forum);
+
+    if (ret == -1) {
+	(void) mysql_free_result(res);
+	return -1;
+    }
+    if (mysql_num_rows(res) == 0) {
+	(void) mysql_free_result(res);
+	return -2;
+    }
+    if (mysql_num_rows(res) > 1) {
+	(void) mysql_free_result(res);
+	return -3;
+    }
+    row = mysql_fetch_row(res);
+
+    /*
+     * Determine size.
+     */
+    memset(&message, 0, sizeof(message_t));
+    message.content = (char *) xmalloc(strlen(row[7]));
+    memset(message.content, 0, sizeof(message.content));
+
+    /*
+     * Admin info
+     */
+    message.num = atoi(row[0]);
+    message.topic = atoi(row[1]);
+    message.forum = atoi(row[2]);
+
+    /*
+     * Header info
+     */
+    message.author = atoi(row[3]);
+    sprintf(message.alias, row[4]);
+    sprintf(message.subject, row[5]);
+    (void) datetime_to_time(row[6], &message.date);
+
+    /*
+     * Message content
+     */
+    sprintf(message.content, row[7]);
+
+    /*
+     * And more admin info.
+     */
+    message.type = atoi(row[8]);;
+    message.priv = atoi(row[9]);
+    message.deleted = row[10];
+
+    (void) mysql_free_result(res);
+
+    *data = message;
 
     return 0;
 }
 
+/*
+ * Returns a linked list containing messages in a forum.
+ * Points both ways, for reading forward and backwards..
+ */
 int
-mono_sql_remove_message(int message_id)
+mono_sql_mes_list_forum(unsigned int forum, unsigned int start, mlist_t ** list)
 {
 
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+    int ret = 0, rows = 0, i = 0;
+    mlist_t entry;
+
+    ret = mono_sql_query(&res, "SELECT (message_id) FROM " M_TABLE " WHERE forum_id=%d AND message_id>=%d AND deleted=0 ORDER BY message_id", forum, start);
+
+    if (ret == -1) {
+	(void) mysql_free_result(res);
+	return -1;
+    }
+    if ((rows = mysql_num_rows(res)) == 0) {
+	(void) mysql_free_result(res);
+	return -1;
+    }
+    for (i = 0; i < rows; i++) {
+	row = mysql_fetch_row(res);
+	if (row == NULL)
+	    break;
+	/* Get message_id */
+	if (sscanf(row[0], "%u", &(entry.id)) == -1)
+	    continue;
+	/* Add this one to the linked list */
+        cprintf("DEBUG: Added message %u.\n", entry.id);
+	if (_mono_sql_add_num_to_list(entry, list) == -1)
+	    break;
+    }
+    mysql_free_result(res);
     return 0;
 }
 
+/*
+ * Returns a linked list containing messages in a topic.
+ * Points both ways, for reading forward and backwards..
+ */
 int
-mono_sql_retrieve_message(int message_id)
+mono_sql_mes_list_topic(unsigned int topic, unsigned int start, mlist_t ** list)
 {
 
+    MYSQL_RES *res;
+    MYSQL_ROW row;
+    int ret = 0, rows = 0, i = 0;
+    mlist_t entry;
+
+    ret = mono_sql_query(&res, "SELECT FROM " M_TABLE " WHERE topic_id=%d AND message_id>=%d AND deleted=0 ORDER BY message_id", topic, start);
+
+    if (ret == -1) {
+	(void) mysql_free_result(res);
+	return -1;
+    }
+    if ((rows = mysql_num_rows(res)) == 0) {
+	(void) mysql_free_result(res);
+	return -1;
+    }
+    for (i = 0; i < rows; i++) {
+	row = mysql_fetch_row(res);
+	if (row == NULL)
+	    break;
+	/* Get message_id */
+	if (sscanf(row[0], "%u", &(entry.id)) == -1)
+	    continue;
+	/* Add this one to the linked list */
+	if (_mono_sql_add_num_to_list(entry, list) == -1)
+	    break;
+    }
+    mysql_free_result(res);
+    return 0;
+
+}
+
+/*
+ * Add an entry to the message linked list.
+ * Provide support for going in both directions.
+ *
+ * God, I HATE linked lists.
+ */
+static int
+_mono_sql_mes_add_num_to_list(mlist_t entry, mlist_t ** list)
+{
+
+    mlist_t *p, *q;
+
+    /*
+     * Note mono_sql_free_list()
+     */
+    p = (mlist_t *) xmalloc(sizeof(mlist_t));
+    if (p == NULL)
+	return -1;
+
+    *p = entry;
+    p->next = NULL;
+    p->prev = NULL;
+
+    q = *list;
+    if (q == NULL) {
+	*list = p;
+    } else {
+	while (q->next != NULL)
+	    q = q->next;
+	q->next = p;
+        p->prev = q;
+    }
     return 0;
 }
 
+void
+mono_sql_mes_free_list(mlist_t *list)
+{
+
+    mlist_t *ref;
+
+    while (list != NULL) {
+        ref = list->next;
+        xfree(list);
+        list = ref;
+    }
+    return;
+}
 
 /* eof */
