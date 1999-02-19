4a85f8f2da55aa36bfb2e49637afc2a95b232dba
Michel Oosterhof <michel@oosterhof.net>
1999-02-19 10:37:52 +0000

    new passwd funcs

--- include/sql_user.h
+++ include/sql_user.h
@@ -6,5 +6,8 @@ extern int mono_sql_u_add_user(const char *username);
 extern int mono_sql_u_kill_user(unsigned int user_id);
 extern int mono_sql_u_id2name(unsigned int userid, char *username);
 extern int mono_sql_u_name2id(const char *username, unsigned int *userid);
-int mono_sql_u_add_user_new(const char *, unsigned int );
+extern int mono_sql_u_add_user_new(const char *, unsigned int );
+
+extern int mono_sql_u_set_passwd( unsigned int, const char * );
+extern int mono_sql_u_check_passwd( unsigned int, const char * );
 /* eof */
