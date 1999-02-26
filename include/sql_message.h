ee27c20e8ae4a03c566e75e161bd0a9b600d039b
Rusell Crandall <russ@monolith>
1999-02-26 21:56:47 +0000

    added missing prototypes

--- include/sql_message.h
+++ include/sql_message.h
@@ -6,5 +6,6 @@ extern int mono_sql_mes_remove(unsigned int id, unsigned int forum);
 extern int mono_sql_mes_retrieve(unsigned int id, unsigned int forum, message_t *message);
 extern int mono_sql_mes_list_forum(unsigned int forum, unsigned int start, mlist_t ** list);
 extern int mono_sql_mes_list_topic(unsigned int topic, unsigned int start, mlist_t ** list);
+extern void mono_sql_mes_free_list(mlist_t *);
 
 #define M_TABLE "message"
