5fdf9108df7f3d58ce8b94df3497ca8e15d4e9c5
Peter Lieverdink <me@cafuego.net>
1999-02-27 02:38:05 +0000

    Fuxed mysql.h include.

--- shix/libshix.c
+++ shix/libshix.c
@@ -33,7 +33,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include <mysql.h>
+#ifdef HAVE_MYSQL_H
+  #undef HAVE_MYSQL_MYSQL_H
+  #include <mysql.h>
+#else
+  #ifdef HAVE_MYSQL_MYSQL_H
+    #undef HAVE_MYSQL_H
+    #include <mysql/mysql.h>
+  #endif
+#endif
 
 #include "monolith.h"
 #include "libmono.h" 
