a8fffc26fa12423ccbd2f88813dfe6b65894aacb
Peter Lieverdink <me@cafuego.net>
1999-04-01 13:06:27 +0000

    Fixed up for final version.

--- doc/sql/message.sql
+++ doc/sql/message.sql
@@ -4,19 +4,41 @@ DROP TABLE message;
 
 CREATE TABLE message (
 
+   # Message info.
    message_id	INT UNSIGNED NOT NULL,
    topic_id	INT UNSIGNED NOT NULL,
    forum_id	INT UNSIGNED NOT NULL,
 
-   author	INT UNSIGNED,
-   alias	VARCHAR(20) NOT NULL,
-
-   subject	VARCHAR(100),
+   # Extra display info.
+   flag		ENUM( 'normal','anon','alias','forced','yell','auto','roomaide','tech','sysop','emp' ),
    date		DATETIME NOT NULL,
 
-   type		ENUM( 'anon', 'alias', 'normal' ),
-   priv		ENUM( 'emp', 'sysop', 'tech', 'host', 'normal' ),
+   # Actual useful info.
+   author	INT UNSIGNED NOT NULL,
+   alias	VARCHAR(20) NOT NULL,
+   subject	VARCHAR(100),
+   content	BLOB NOT NULL,
+
    deleted	ENUM( 'y', 'n' ),
+
+   # Reply info.
+   r_messge_id	INT UNSIGNED,
+   r_forum_id	INT UNSIGNED,
+   r_topic_id	INT UNSIGNED,
+   r_flag	ENUM( 'normal','anon','alias','forced','yell','auto','roomaide','tech','sysop','emp' ),
+   r_date	DATETIME,
+   r_author	INT UNSIGNED,
+   r_alias	VARCHAR(20),
+   r_subject	VARCHAR(100),
+
+   # Modification info.
+   m_messge_id	INT UNSIGNED,
+   m_forum_id	INT UNSIGNED,
+   m_topic_id	INT UNSIGNED,
+   m_date	TIMESTAMP,
+   m_flag	ENUM( 'normal','anon','alias','forced','yell','auto','roomaide','tech','sysop','emp' ),
+   m_author	INT UNSIGNED,
+   m_reason	ENUM( 'copied','moved' ),
 
    stamp	TIMESTAMP,
 
