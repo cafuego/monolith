7d768da927565990d4146ad23017fe79309fdc27
Rusell Crandall <russ@monolith>
1999-04-02 16:59:01 +0000

    *** empty log message ***

---
+++ include/enter_message.h
@@ -0,0 +1,5 @@
+extern int enter_message(unsigned int, int, unsigned long, const char *);
+extern void enter_admin_message(void);
+extern void enter_yell(void);
+extern void change_forum_info(void);
+extern void i_deleted_your_post_haha_mail(const unsigned int);
