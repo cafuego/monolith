d72dd2b254479c573fb7961ad75310680724e44d
Peter Lieverdink <me@cafuego.net>
1999-04-05 11:41:49 +0000

    *sigh*

--- src/read_menu.c
+++ src/read_menu.c
@@ -1189,7 +1189,7 @@ bingle(unsigned int forum)
     if(!(quad.flags & QR_INUSE ))
         return;
 
-    cprintf("\rWorking on %d.%s", i, quad.name); fflush(stdout);
+    cprintf("\rWorking on %d.%s", forum, quad.name); fflush(stdout);
     for (i = quad.lowest; i <= quad.highest; i++) {
        (void)copy_message_to_sql(forum,i);
        cprintf("."); fflush(stdout);
