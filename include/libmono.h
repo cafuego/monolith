a16dcf6440ba39af0d333212a252e04f67f185ae
Michel Oosterhof <michel@oosterhof.net>
1999-04-16 08:38:55 +0000

    added stuff

--- include/libmono.h
+++ include/libmono.h
@@ -1,6 +1,7 @@
 /* $Id$ */
 
 #include "btmp.h"
+#include "libcache.h"
 #include "dexi.h"
 #include "libquad.h"
 #include "log.h"
@@ -8,6 +9,11 @@
 #include "userfile.h"
 #include "libfriends.h"
 #include "libuid.h"
+#include "sql_config.h"
+#include "sql_useruser.h"
+#include "sql_userforum.h"
+#include "sql_usertopic.h"
+#include "msg_file.h"
 #include "sql_utils.h"
 #include "sql_user.h"
 
