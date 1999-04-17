f72eade79bd59a91a834f018526651e0d7eb91a5
Peter Lieverdink <me@cafuego.net>
1999-04-17 16:38:37 +0000

    General x-log table.

---
+++ doc/sql/express.sql
@@ -0,0 +1,25 @@
+# global x log.
+
+# Do not drop, contains live data!
+DROP TABLE express;
+
+CREATE TABLE express (
+
+   id		INT UNSIGNED NOT NULL AUTO_INCREMENT,
+   user_id	INT UNSIGNED NOT NULL,
+
+   date		DATETIME NOT NULL,
+   sender	VARCHAR(60),
+   recipient	VARCHAR(60),
+   message	BLOB,
+
+   sender_priv	INT UNSIGNED NOT NULL,
+   override	INT NOT NULL,
+   ack		INT NOT NULL,
+
+   stamp	TIMESTAMP,
+
+   PRIMARY KEY	( id ),
+   INDEX	( user_id ),
+   INDEX	( date )
+)
