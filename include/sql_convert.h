8f5cbc0c5c426770c14ec7450c1f6ebdbb8ef460
Peter Lieverdink <me@cafuego.net>
1999-04-17 16:51:24 +0000

    Added x message into sql support.

--- include/sql_convert.h
+++ include/sql_convert.h
@@ -4,3 +4,4 @@
 
 message_t * mono_sql_convert_row_to_mes(MYSQL_ROW row);
 sr_t * mono_sql_convert_row_to_sr(MYSQL_ROW row);
+express_t * mono_sql_convert_row_to_x(MYSQL_ROW row);
--- include/sql_llist.h
+++ include/sql_llist.h
@@ -6,5 +6,7 @@ extern int mono_sql_ll_add_mlist_to_list(mlist_t entry, mlist_t ** list);
 extern void mono_sql_ll_free_mlist(mlist_t *list);
 extern int mono_sql_ll_add_srlist_to_list(sr_list_t entry, sr_list_t ** list);
 extern void mono_sql_ll_free_sr_list(sr_list_t *list);
+extern int mono_sql_ll_add_xlist_to_list(xlist_t x, xlist_t ** list);
+extern void mono_sql_ll_free_xlist(xlist_t *list);
 
 /* eof */
