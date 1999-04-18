a5dfb57e3626eeaf8e918123ccfc2f78b8a05ae1
Michel Oosterhof <michel@oosterhof.net>
1999-04-18 19:44:42 +0000

    table rename

--- include/sql_topic.h
+++ include/sql_topic.h
@@ -1,4 +1,4 @@
-#define T_TABLE "topic"
+#define T_TABLE "topics"
 
 extern int mono_sql_t_new_topic(unsigned int topic_id, topic_t * top);
 extern int mono_sql_t_rename_topic( unsigned int topic_id, const char *newname );
