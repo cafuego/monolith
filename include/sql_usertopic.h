5fa0357bef05ddcebf4aef6b06a6aabb865afe2d
Michel Oosterhof <michel@oosterhof.net>
1999-05-26 20:43:24 +0000

    some changes in design of usertopic

--- include/sql_usertopic.h
+++ include/sql_usertopic.h
@@ -1,6 +1,6 @@
 /* $Id$ */
 
-extern int mono_sql_ut_add_entry(unsigned int , unsigned int );
-extern int mono_sql_ut_kill_topic(unsigned int );
-extern int mono_sql_ut_kill_user(unsigned int );
+extern int mono_sql_ut_add_entry(unsigned int , unsigned int, unsigned int );
+extern int mono_sql_ut_kill_topic(unsigned int, unsigned int );
+extern int mono_sql_ut_kill_user(unsigned int);
 extern int mono_sql_ut_new_user(unsigned int);
