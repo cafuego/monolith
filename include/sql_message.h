331998517e90c82fbe1649bc9afdfef265b7c49b
Peter Lieverdink <me@cafuego.net>
1999-06-13 08:50:02 +0000

    Added 2 protos to fix compiler warnings.

--- include/sql_message.h
+++ include/sql_message.h
@@ -9,6 +9,7 @@ extern int mono_sql_mes_list_forum(unsigned int forum, unsigned int start, mlist
 extern int mono_sql_mes_list_topic(unsigned int topic, unsigned int start, mlist_t ** list);
 extern int mono_sql_mes_search_forum(int forum, const char *needle, sr_list_t **list);
 extern int mono_sql_mes_mark_deleted(unsigned int id, unsigned int forum);
+extern int mono_sql_mes_erase_forum( unsigned int forum );
 
 #define M_TABLE "message"
 #define FILEDIR BBSDIR "/save/messages"
--- include/sql_usertopic.h
+++ include/sql_usertopic.h
@@ -4,3 +4,7 @@ extern int mono_sql_ut_add_entry(unsigned int , unsigned int, unsigned int );
 extern int mono_sql_ut_kill_topic(unsigned int, unsigned int );
 extern int mono_sql_ut_kill_user(unsigned int);
 extern int mono_sql_ut_new_user(unsigned int);
+extern int mono_sql_ut_update_lastseen( unsigned int user_id, unsigned int forum_id,
+                             unsigned int topic_id, unsigned int message_id );
+extern int mono_sql_ut_query_lastseen( unsigned int user_id, unsigned int forum_id,
+                             unsigned int topic_id, unsigned int *message_id );
