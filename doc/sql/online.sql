0224b4ffa8145518e022d2c81bf85a6e3ff928cc
Peter Lieverdink <me@cafuego.net>
1999-07-22 01:23:39 +0000

    *** empty log message ***

--- doc/sql/online.sql
+++ doc/sql/online.sql
@@ -8,7 +8,7 @@ CREATE TABLE online (
    user_id	INT UNSIGNED NOT NULL,
    status	ENUM( 'yes', 'no' ) DEFAULT 'yes',
    interface	ENUM( 'telnet', 'client', 'web' ) DEFAULT 'telnet',
-   doing	CHAR(30) NOT NULL,
+   doing	CHAR(40) NOT NULL,
    stamp        TIMESTAMP,
 
    PRIMARY KEY( user_id )
