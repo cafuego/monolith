8aa696f49b9ca88f063e7c2f6e38b63e6661043b
Michel Oosterhof <michel@oosterhof.net>
1999-07-28 12:17:00 +0000

    this file contains the private defines in config.h

---
+++ acconfig.h
@@ -0,0 +1,16 @@
+/* bing */
+
+#define ENABLE_NLS
+#define HAVE_ANIMAL
+#define HAVE_CATGETS
+#define HAVE_GETTEXT
+#define HAVE_LC_MESSAGES
+#define HAVE_MAP_FAILED
+#define HAVE_MYSQL_H
+#define HAVE_MYSQL_MYSQL_H
+#define HAVE_STPCPY
+#define HAVE_SYS_MMAN_H
+#define LINUX
+#define SUPERHERO
+#define USE_MYSQL
+#define USE_RATING
