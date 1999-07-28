7c464daa0dfcf0649ace8255e244e1af8f27f294
Michel Oosterhof <michel@oosterhof.net>
1999-07-28 12:19:37 +0000

    updated

--- acconfig.h
+++ acconfig.h
@@ -1,16 +1,26 @@
 /* bing */
 
-#define ENABLE_NLS
-#define HAVE_ANIMAL
-#define HAVE_CATGETS
-#define HAVE_GETTEXT
-#define HAVE_LC_MESSAGES
-#define HAVE_MAP_FAILED
-#define HAVE_MYSQL_H
-#define HAVE_MYSQL_MYSQL_H
-#define HAVE_STPCPY
-#define HAVE_SYS_MMAN_H
-#define LINUX
-#define SUPERHERO
-#define USE_MYSQL
-#define USE_RATING
+/* Define if your locale.h file contains LC_MESSAGES.  */
+#undef HAVE_LC_MESSAGES
+
+/* Define to 1 if NLS is requested.  */
+#undef ENABLE_NLS
+
+/* Define as 1 if you have catgets and don't want to use GNU gettext.  */
+#undef HAVE_CATGETS
+
+/* Define as 1 if you have gettext and don't want to use GNU gettext.  */
+#undef HAVE_GETTEXT
+
+/* Define as 1 if you have the stpcpy function.  */
+#undef HAVE_STPCPY
+
+#undef HAVE_ANIMAL
+#undef HAVE_MAP_FAILED
+#undef HAVE_MYSQL_H
+#undef HAVE_MYSQL_MYSQL_H
+#undef HAVE_SYS_MMAN_H
+#undef LINUX
+#undef SUPERHERO
+#undef USE_MYSQL
+#undef USE_RATING
