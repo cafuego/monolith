2365c783f1f3329873e4985505931cddb58062cf
Peter Lieverdink <me@cafuego.net>
1999-07-30 01:44:15 +0000

    *** empty log message ***

--- web/script/global.js
+++ web/script/global.js
@@ -10,7 +10,7 @@ function logout( url ) {
     // If not to a link on the BBS pages, then we go and
     // visit our logout page.
     //
-    if( url.indexOf("/bbs/") != 0 )
+    if( url.indexOf("/bbs/") == -1 )
         open("/bbs/index.phtml/q");
 
 }
