1cc844baa8e64c6fb7041abc1050e9e391464b09
Peter Lieverdink <me@cafuego.net>
1999-07-30 02:05:10 +0000

    *** empty log message ***

--- web/script/global.js
+++ web/script/global.js
@@ -12,10 +12,8 @@ function logout() {
     // If not to a link on the BBS pages, then we go and
     // visit our logout page.
     //
-    alert(location);
-    url = location;
-    alert(url);
-    if( url.indexOf('/bbs/') == -1 ) {
+    alert(window.location);
+    if( window.location.indexOf('/bbs/') == -1 ) {
         confirm("You are leaving Monolith BBS\n\nYou must log out first and will\nnow be taken to the logout page.");
         self.location = "/bbs/index.phtml/q";
     }
