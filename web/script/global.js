bf1faab79cae0d0dde6419b2df11c731e676ac4b
Peter Lieverdink <me@cafuego.net>
1999-07-30 01:40:47 +0000

    *** empty log message ***

--- web/script/global.js
+++ web/script/global.js
@@ -4,14 +4,21 @@
  * Global JavaScript functions for BBS Web Interface.
  */
 
-function logout() {
+function logout( url ) {
 
-    open("/bbs/index.phtml/q");
+    // Check where we're going.
+    // If not to a link on the BBS pages, then we go and
+    // visit our logout page.
+    //
+    if( url.indexOf("/bbs/") == 0 )
+        open("/bbs/index.phtml/q");
 
 }
 
 function setStatus( text ) {
 
+    // Set status window content to `text'.
+    //
     window.status = text;
     return true;
 
