b2d537b32e1ac329051656daf5c2a2c99b564ce0
Peter Lieverdink <me@cafuego.net>
1999-08-05 05:47:14 +0000

    *** empty log message ***

--- include/uadmin.h
+++ include/uadmin.h
@@ -1,7 +1,7 @@
 /* $Id$ */
 
 /* prototypes */
-extern void useradmin ( void );
+extern void useradmin ( const char * );
 extern void DELETE_DELMARKED_USERS( void );
 extern void update_all_users_read_from_userdir( void );
 extern void deluserfromw (void);      
