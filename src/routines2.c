5752fe44210a76ecc00771b72cff27c25fd563f3
Peter Lieverdink <me@cafuego.net>
1999-08-07 10:57:20 +0000

    *** empty log message ***

--- src/routines2.c
+++ src/routines2.c
@@ -366,7 +366,8 @@ print_system_config()
     cprintf("\1wMessages currently in database:\1g %d, %d posted by you ", total, mine);
     printf("(%.2f%%)\n", percent);
 #endif
-    cprintf("\n\1gBuilt in `%s' on %s \1f\1w(\1g#%d\1w)\n", BUILD_DIR, printdate(buf.st_mtime, 0), COMPILE_NUM);
+    cprintf("\n\1wLast compiled                 :\1g %s\1f\n", printdate(buf.st_mtime, 0) );
+    cprintf("\1gBuilt in                      : %s \1w(\1g#%d\1w)\n", BUILD_DIR, COMPILE_NUM);
     cprintf("\1wMachine load                  :\1g ");
     printf("%.2f %.2f %.2f", l1, l2, l3);
     cprintf(" \1w(\1g%d\1w/\1g%d\1w)\n", aproc, iproc);
