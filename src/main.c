9f197763ca11fc78dea0600133954c91552cc4b1
Peter Lieverdink <me@cafuego.net>
1999-08-31 00:21:07 +0000

    *** empty log message ***

--- src/main.c
+++ src/main.c
@@ -219,11 +219,14 @@ enter_passwd(const char *username)
     (void) getline(pwtest, -19, 1);
 
     if (strlen(pwtest) == 0) {
+        xfree(user);
 	failures++;
 	return FALSE;
     }
-    if (mono_sql_u_check_passwd(user->usernum, pwtest) == TRUE)
+    if (mono_sql_u_check_passwd(user->usernum, pwtest) == TRUE) {
+        xfree(user);
 	return TRUE;
+    }
 
     cprintf(_("Incorrect login.\n"));
     log_it("badpw", "%s from %s", username, hname);
@@ -232,6 +235,7 @@ enter_passwd(const char *username)
 	cprintf("Too many failures.\n");
 	logoff(-1);
     }
+    xfree(user);
     return FALSE;
 }
 
