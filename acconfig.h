b4402c1e8784977efc6efbf550447cc8d6b9ab1a
Michel Oosterhof <michel@oosterhof.net>
1999-09-15 07:26:04 +0000

    needs HAVE_SENDMAIL

--- acconfig.h
+++ acconfig.h
@@ -18,9 +18,14 @@
 #undef HAVE_ANIMAL
 #undef HAVE_MAP_FAILED
 #undef HAVE_MYSQL_H
-#undef HAVE_MYSQL_MYSQL_H
+#undef MYSQL_DIR
+#undef MYSQL_INCLUDE
+#undef MYSQL_LIB
 #undef HAVE_SYS_MMAN_H
 #undef LINUX
 #undef SUPERHERO
 #undef USE_MYSQL
 #undef USE_RATING
+
+/* Has Sendmail */
+#undef HAVE_SENDMAIL
