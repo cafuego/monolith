b518aef2415ab9be25b1bd194a84e092f495db90
Michel Oosterhof <michel@oosterhof.net>
1999-10-20 14:27:43 +0000

    removed deprecated function

--- include/libquad.h
+++ include/libquad.h
@@ -2,7 +2,6 @@
 
 /* prototypes */
 extern int change_QL(unsigned int, const char *, int);
-extern room_t read_quad(unsigned int);
 extern int read_forum( unsigned int, room_t * );
 extern int write_forum( unsigned int, room_t * );
 extern int write_quad(room_t, int);
